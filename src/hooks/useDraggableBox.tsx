7a85174a34d852da587759114c6568e7249a82e8
pabrcno <pabrcn@gmail.com>
2023-08-14 12:53:58 -0300

    improvements to rotations and controls

--- src/hooks/useDraggableBox.tsx
+++ src/hooks/useDraggableBox.tsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from "react";
 import { useThree } from "@react-three/fiber";
 import { useGesture } from "@use-gesture/react";
 import { PublicApi, Triplet, useBox } from "@react-three/cannon";
@@ -10,15 +11,20 @@ interface UseDraggableBoxProps {
   proportions?: Triplet;
 }
 
-export const useDraggableBox = ({
-  mass = 0.025,
-  friction = 100,
-  restitution = 0,
-  proportions = [1, 1, 1],
-}: UseDraggableBoxProps) => {
+export const useDraggableBox = (props: UseDraggableBoxProps) => {
+  const {
+    mass = 0.025,
+    friction = 100,
+    restitution = 0,
+    proportions = [1, 1, 1],
+  } = props;
+
   const { size, viewport } = useThree();
   const aspect = size.width / viewport.width;
 
+  const [position, setPosition] = useState<Triplet>([0, 0, 0]); // State to track the position of the object
+
+  const [isDragging, setIsDragging] = useState(false); // State to track if the object is being dragged
   const [ref, api] = useBox(() => ({
     args: proportions,
     mass,
@@ -28,23 +34,44 @@ export const useDraggableBox = ({
 
   const bind = useGesture({
     onDragStart: () => {
+      setIsDragging(true); // Set drag state to true
       api.mass.set(0);
-      api.rotation.set(0, 0, 0);
+      api.rotation.set(0, ref.current?.rotation.y ?? 0, 0);
       api.velocity.set(0, 0, 0);
       api.angularVelocity.set(0, 0, 0);
     },
     onDrag: ({ offset: [x, y] }) => {
-      const position: Triplet = [
-        x / aspect,
-        -y / aspect,
-        ref.current?.position.z ?? 0,
-      ];
-      api.position.set(...position);
+      api.position.set(...[x / aspect, -y / aspect, 0]);
     },
     onDragEnd: () => {
+      setIsDragging(false); // Set drag state to false
       api.mass.set(mass);
     },
   });
+
+  useEffect(() => {
+    const handleKeydown = (event: KeyboardEvent) => {
+      if (!ref.current || !isDragging) return; // Check if the object is being dragged
+
+      switch (event.key.toLowerCase()) {
+        case "a":
+          api.rotation.set(0, (ref.current.rotation.y -= 0.1), 0);
+          break;
+        case "d":
+          api.rotation.set(0, (ref.current.rotation.y += 0.1), 0);
+          break;
+        default:
+          break;
+      }
+    };
+
+    window.addEventListener("keydown", handleKeydown);
+
+    return () => {
+      // Cleanup event listener on component unmount
+      window.removeEventListener("keydown", handleKeydown);
+    };
+  }, [ref, isDragging]); // Add isDragging as a dependency
 
   return { ref, bind };
 };
