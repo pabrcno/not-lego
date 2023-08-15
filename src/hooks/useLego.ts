6cf679cb4c814bc98bfdd6c0057247734ae159d0
pabrcno <pabrcn@gmail.com>
2023-08-15 12:27:30 -0300

    remove magnetic bs

--- src/hooks/useLego.ts
+++ src/hooks/useLego.ts
@@ -35,39 +35,11 @@ export const useLego = (props: UseLegoProps) => {
       debouncedPlay();
     }
   };
-  const magneticForce = 10; // Define a constant for the strength of the magnetic attraction.
 
-  const applyMagneticForce = (e: CollideEvent) => {
-    if (!ref.current) return;
-    const otherBox = e.body;
-
-    const ourBoxPosition = ref.current.position.clone();
-    const otherBoxPosition = otherBox.position.clone();
-
-    const isAbove = ourBoxPosition.y > otherBoxPosition.y;
-
-    // Check if the boxes are in proximity
-    const distance = isAbove
-      ? ourBoxPosition.y - (props.proportions?.[1] ?? 1) - otherBoxPosition.y
-      : otherBoxPosition.y - (props.proportions?.[1] ?? 1) - ourBoxPosition.y;
-
-    if (distance < 0.1) {
-      // 0.1 is a threshold, adjust based on your requirements
-      const forceMagnitude = magneticForce / (distance + 0.01); // Add a small value to avoid division by zero
-
-      const forceDirection = new THREE.Vector3(0, isAbove ? -1 : 1, 0); // Up or down depending on position
-      const force = forceDirection.multiplyScalar(forceMagnitude);
-
-      // Apply the forces
-      api.applyForce(force.toArray(), ourBoxPosition.toArray());
-    }
-  };
-
-  const { ref, api, bind, isDragging } = useDraggableBox({
+  const { ref, bind } = useDraggableBox({
     ...props,
     onCollide: (e) => {
       playAudio(e);
-      applyMagneticForce(e);
     },
   });
 
